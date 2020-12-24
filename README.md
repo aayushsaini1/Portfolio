032f8c8ffb2dfb6c50868a145fab955d7cb1d790
Aayush Saini <aayushsaini.77@gmail.com>
2020-12-24 14:47:53 +0530

    Revert "added ss of website to readme"
    This reverts commit fa5cd4b5f592257fb2c82797405c0cbab52b4047.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Aayush Saini - Product Designer | 3D Artist
 
 This is my current portfolio website.
-
-![GitHub Logo](/assets/portfolio-ss.png)
---
+++ script.js
@@ -0,0 +1,15 @@
+function darkMode() {
+
+    var element = document.body;
+    element.classList.toggle("dark");
+
+    var navbar = document.body;
+    navbar.classList.toggle("dark-mode");
+
+    var btn = document.body;
+    btn.classList.toggle("dark-btn");
+
+    var icons = document.body;
+    icons.classList.toggle("dark-list");
+
+}
